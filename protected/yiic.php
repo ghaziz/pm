c43a10ffe5cadadf2d85874375fdf9caf31859b8
ebrahim <ebrahim.poursadeqi@gmail.com>
2015-03-01 01:33:00 -0800

    ghazi pull ver 1

--- protected/yiic.php
+++ protected/yiic.php
@@ -1,5 +1,5 @@
 <?php
-
+//this line is only for test
 // change the following paths if necessary
 $yiic=dirname(__FILE__).'/../../yii-1.1.15.022a51/framework/yiic.php';
 $config=dirname(__FILE__).'/config/console.php';
